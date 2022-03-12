2189a92ebcf0b8ef21299ed41392f7f2398af28c
Meme <meme_es@hotmail.com>
2022-03-12 10:05:48 -0600

    Add Constant class

---
+++ app/src/main/java/com/example/postretriever/utils/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.postretriever.utils
+
+object Constants {
+    const val BASE_URL = "https://jsonplaceholder.typicode.com/"
+    const val EXTRA_POST_TITLE = "com.example.postretriever.POST_TITLE"
+    const val EXTRA_POST_ID = "com.example.postretriever.POST_ID"
+}
